dd9d10fd1c510b1e68227758183ecda009f9e4cf
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-09-27 17:56:26 -0300

    feat: implement speech widget

---
+++ script.js
@@ -0,0 +1,109 @@
+const TEXTS = {
+  MAIN: "TALK TO ME",
+  STOP: "STOP TALKING",
+  TALKING: "TALKING...",
+  EMPTY_CLIPBOARD: "NOTHING TO TALK!",
+};
+
+const WRAPPER_ID = "speech-wrapper";
+const SPEECH_BUTTON_ID = "speech-button";
+const STOP_SPEECH_BUTTON_ID = "stop-speech-button";
+
+function createSpeechButtons() {
+  const wrapper = document.createElement("div");
+  const speechButton = document.createElement("button");
+  const stopSpeechButton = document.createElement("button");
+
+  wrapper.id = WRAPPER_ID;
+
+  speechButton.id = SPEECH_BUTTON_ID;
+  speechButton.innerText = TEXTS.MAIN;
+  setButtonColors(speechButton);
+
+  stopSpeechButton.id = STOP_SPEECH_BUTTON_ID;
+  stopSpeechButton.innerText = TEXTS.STOP;
+  stopSpeechButton.disabled = true;
+  setButtonColors(stopSpeechButton);
+
+  speechButton.addEventListener(
+    "click",
+    handleSpeechButtonClick.bind(null, speechButton, stopSpeechButton)
+  );
+
+  stopSpeechButton.addEventListener(
+    "click",
+    handleStopSpeechButtonClick.bind(null, speechButton, stopSpeechButton)
+  );
+
+  wrapper.appendChild(speechButton);
+  wrapper.appendChild(stopSpeechButton);
+
+  document.body.appendChild(wrapper);
+}
+
+async function handleSpeechButtonClick(speechButton, stopSpeechButton) {
+  const selectedText = await getText();
+
+  if (!selectedText) return handleEmptyClipboard(speechButton);
+
+  const speech = prepareSpeech(selectedText);
+
+  speech.instance.onstart = () => {
+    speechButton.innerText = TEXTS.TALKING;
+    speechButton.disabled = true;
+    stopSpeechButton.disabled = false;
+  };
+
+  speech.instance.onend = () => {
+    speechButton.innerText = TEXTS.MAIN;
+    speechButton.disabled = false;
+    stopSpeechButton.disabled = true;
+  };
+
+  return speech.start();
+}
+
+function handleStopSpeechButtonClick(speechButton, stopSpeechButton) {
+  window.speechSynthesis.cancel();
+
+  speechButton.innerText = TEXTS.MAIN;
+  speechButton.disabled = false;
+  stopSpeechButton.disabled = true;
+}
+
+async function getText() {
+  try {
+    const selectedText = await navigator.clipboard.readText();
+
+    return selectedText;
+  } catch (e) {
+    console.error("Clipboard API is not supported");
+  }
+}
+
+function handleEmptyClipboard(speechButton) {
+  speechButton.innerText = TEXTS.EMPTY_CLIPBOARD;
+
+  const TWO_SECONDS = 2000;
+
+  setTimeout(() => {
+    speechButton.innerText = TEXTS.MAIN;
+  }, TWO_SECONDS);
+}
+
+function prepareSpeech(text) {
+  const instance = new SpeechSynthesisUtterance(text);
+
+  function start() {
+    window.speechSynthesis.speak(instance);
+  }
+
+  return { instance, start };
+}
+
+function setButtonColors(button) {
+  button.style.backgroundColor = "#333333";
+  button.style.color = "#ffffff";
+}
+
+window.addEventListener("load", createSpeechButtons);
